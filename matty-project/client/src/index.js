702550342b9d42d7bd4eb6b64265053094d5b7fc
Kritika Ekka <121709627+Kritikaekka@users.noreply.github.com>
2025-10-03 18:12:02 +0530

    Update index.js

--- matty-project/client/src/index.js
+++ matty-project/client/src/index.js
@@ -1,11 +1,15 @@
+
+import './styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles.css';
+import { AuthProvider } from './context/AuthContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
